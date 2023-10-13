e66cf5e9150250ddf10b4c363d37b202fa8d4330
Jeppe <jfrimannlarsen@gmail.com>
2023-10-13 13:24:15 +0200

    yes

--- CheckJsonFiles.py
+++ CheckJsonFiles.py
@@ -1,5 +1,27 @@
 
 
 class checkJsonFiles():
-    def FileCheck(self):
-        pass
+    def FileCheck(self, name):
+        name
+        image
+        pronunciation
+        name
+        meaning
+        type of dinosuar
+        length
+        diet
+        when
+        it
+        lived
+        found in
+        taconomy
+        named
+        by
+        type species
+
+        teeth
+        weight
+        Food
+        how
+        it
+        moved
